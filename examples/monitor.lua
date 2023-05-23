38e0d1393fe6b2057a068a13b5edca49d4076919
Matthew Blewitt <blewitt.matt@gmail.com>
2023-05-23 12:22:52 +0100

    maint: format with StyLua
    This commit formats tests and the library itself with StyLua, and adds
    this to the actions run for linting.

--- examples/monitor.lua
+++ examples/monitor.lua
@@ -1,11 +1,11 @@
 package.path = "../src/?.lua;src/?.lua;" .. package.path
 pcall(require, "luarocks.require")
 
-local redis = require 'redis'
+local redis = require("redis")
 
 local params = {
-    host = '127.0.0.1',
-    port = 6379,
+	host = "127.0.0.1",
+	port = 6379,
 }
 
 local client = redis.connect(params)
@@ -16,18 +16,18 @@ client:select(15) -- for testing purposes
 
 local counter = 0
 for msg, abort in client:monitor_messages() do
-    counter = counter + 1
+	counter = counter + 1
 
-    local feedback = string.format("[%d] Received %s on database %d", msg.timestamp, msg.command, msg.database)
-    if msg.arguments then
-        feedback = string.format('%s with arguments %s', feedback, msg.arguments)
-    end
+	local feedback = string.format("[%d] Received %s on database %d", msg.timestamp, msg.command, msg.database)
+	if msg.arguments then
+		feedback = string.format("%s with arguments %s", feedback, msg.arguments)
+	end
 
-    print(feedback)
+	print(feedback)
 
-    if counter == 5 then
-        abort()
-    end
+	if counter == 5 then
+		abort()
+	end
 end
 
 print(string.format("Closed the MONITOR context after receiving %d commands.", counter))
